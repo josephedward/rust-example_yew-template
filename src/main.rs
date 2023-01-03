8faacfd66e3a21691e884b29db7fb837b894c64d
josephedward <15126922+josephedward@users.noreply.github.com>
2023-01-02 23:41:36 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,104 @@
+
 use yew::prelude::*;
+use yew_router::prelude::*;
 
-#[function_component]
-fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
 
-    html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-        </div>
+// ===================================================================================
+// for {username}.github.io/{repo_name}
+// replace 'yew-template-for-github.io' to your repo name
+
+#[derive(Clone, Routable, PartialEq)]
+enum RootRoute {
+    #[at("/rust-example_yew-template/")]
+    Home,
+    #[at("/rust-example_yew-template/:s")]
+    Route,
+}
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/rust-example_yew-template/about")]
+    About,
+    #[not_found]
+    #[at("/rust-example_yew-template/404")]
+    NotFound,
+}
+
+fn root_route(routes: &RootRoute) -> Html {
+    match routes {
+        RootRoute::Home => html! { 
+            //render a list of BrowserRouter links
+            <div>
+                <p class="text-4xl">{ "Yew Template" }</p>
+                <BrowserRouter>
+                    <ul>
+                        <li><a href="www.google.com">{"google"}</a></li>
+                        // <li><Link<Browser> route={Route::About}>{ "About" }</Link<Route>></li>
+                        // <li><Link<Route> route={Route::NotFound}>{ "Not Found" }</Link<Route>></li>
+                    </ul>
+                </BrowserRouter>
+            </div>
+
+        
+            
+         },
+        RootRoute::Route =>
+            html! {
+            <Switch<Route> render={Switch::render(switch)} />
+        },
     }
 }
 
-fn main() {
-    yew::Renderer::<App>::new().render();
+
+
+fn switch(routes: &Route) -> Html {
+    match routes {
+        Route::About => html! { <p>{ "About" }</p> },
+        Route::NotFound => html! { <p>{ "Not Found" }</p> },
+    }
 }
-// use yew::prelude::*;
-// use yew_router::prelude::*;
 
-
-// // ===================================================================================
-// // for {username}.github.io/{repo_name}
-// // replace 'yew-template-for-github.io' to your repo name
+// ===================================================================================
+// for {username}.github.io
 
 // #[derive(Clone, Routable, PartialEq)]
-// enum RootRoute {
-//     #[at("/rust-example_yew-template/")]
-//     Home,
-//     #[at("/rust-example_yew-template/:s")]
-//     Route,
-// }
+//  enum RootRoute {
+//      #[at("/")]
+//      Home,
+//      #[at("/about")]
+//      About,
+//      #[not_found]
+//      #[at("/404")]
+//      NotFound,
+//  }
 
-// #[derive(Clone, Routable, PartialEq)]
-// enum Route {
-//     #[at("/rust-example_yew-template/about")]
-//     About,
-//     #[not_found]
-//     #[at("/rust-example_yew-template/404")]
-//     NotFound,
-// }
+//  fn root_route(routes: &Route) -> Html {
+//      match routes {
+//          RootRoute::Home => html! { <p class="text-4xl">{ "Yew Template" }</p> },
+//          RootRoute::About => html! { <p>{ "About" }</p> },
+//          RootRoute::NotFound => html! { <p>{ "Not Found" }</p> },
+//      }
+//  }
 
-// fn root_route(routes: &RootRoute) -> Html {
-//     match routes {
-//         RootRoute::Home => html! { 
-//             //render an iframe
-//             <iframe src="https://josephedward.github.io/sci-fi_portfolio/" width="100%vw" height="950px"  />
-//          },
-//         RootRoute::Route =>
-//             html! {
-//             <Switch<Route> render={Switch::render(switch)} />
-//         },
-//     }
-// }
+// ===================================================================================
 
+/// main root
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        // ********************************************************
+        // **    basename is not supported on yew 0.19.0 yet.    **
+        // <BrowserRouter basename="/rust-example_yew-template/">
+        //     <Switch<Route> render={Switch::render(switch)} />
+        // </BrowserRouter>
+        // ********************************************************
+        <BrowserRouter>
+            <Switch<RootRoute> render={Switch::render(root_route)} />
+        </BrowserRouter>
+    }
+}
 
-
-// fn switch(routes: &Route) -> Html {
-//     match routes {
-//         Route::About => html! { <p>{ "About" }</p> },
-//         Route::NotFound => html! { <p>{ "Not Found" }</p> },
-//     }
-// }
-
-// // ===================================================================================
-// // for {username}.github.io
-
-// // #[derive(Clone, Routable, PartialEq)]
-// //  enum RootRoute {
-// //      #[at("/")]
-// //      Home,
-// //      #[at("/about")]
-// //      About,
-// //      #[not_found]
-// //      #[at("/404")]
-// //      NotFound,
-// //  }
-
-// //  fn root_route(routes: &Route) -> Html {
-// //      match routes {
-// //          RootRoute::Home => html! { <p class="text-4xl">{ "Yew Template" }</p> },
-// //          RootRoute::About => html! { <p>{ "About" }</p> },
-// //          RootRoute::NotFound => html! { <p>{ "Not Found" }</p> },
-// //      }
-// //  }
-
-// // ===================================================================================
-
-// /// main root
-// #[function_component(App)]
-// fn app() -> Html {
-//     html! {
-//         // ********************************************************
-//         // **    basename is not supported on yew 0.19.0 yet.    **
-//         // <BrowserRouter basename="/rust-example_yew-template/">
-//         //     <Switch<Route> render={Switch::render(switch)} />
-//         // </BrowserRouter>
-//         // ********************************************************
-//         <BrowserRouter>
-//             <Switch<RootRoute> render={Switch::render(root_route)} />
-//         </BrowserRouter>
-//     }
-// }
-
-// /// entry point
-// fn main() {
-//     yew::start_app::<App>();
-// }
+/// entry point
+fn main() {
+    yew::start_app::<App>();
+}
