25ec212a932f905157be282f2626279d905b6306
josephedward <15126922+josephedward@users.noreply.github.com>
2023-01-02 23:04:05 -0500

    testing yew

--- src/main.rs
+++ src/main.rs
@@ -1,108 +1,24 @@
-
-#![allow(clippy::use_self)]
-
-use enclose::enc;
-use indexmap::IndexMap;
-use seed::{prelude::*, *};
-use serde::{Deserialize, Serialize};
-use std::mem;
-use uuid::Uuid;
-use web_sys::HtmlInputElement;
 use yew::prelude::*;
-use yew_router::prelude::*;
 
-const ENTER_KEY: u32 = 13;
-const ESC_KEY: u32 = 27;
-const STORAGE_KEY: &str = "seed-todomvc";
+#[function_component]
+fn App() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
 
-type TodoId = Uuid;
-
-
-
-// ===================================================================================
-// for {username}.github.io/{repo_name}
-// replace 'yew-template-for-github.io' to your repo name
-
-#[derive(Clone, Routable, PartialEq)]
-enum RootRoute {
-    #[at("/rust-example_yew-template/")]
-    Home,
-    #[at("/rust-example_yew-template/:s")]
-    Route,
-}
-
-#[derive(Clone, Routable, PartialEq)]
-enum Route {
-    #[at("/rust-example_yew-template/about")]
-    About,
-    #[not_found]
-    #[at("/rust-example_yew-template/404")]
-    NotFound,
-}
-
-fn root_route(routes: &RootRoute) -> Html {
-    match routes {
-        RootRoute::Home => html! { 
-            //render an iframe
-            <iframe src="https://josephedward.github.io/sci-fi_portfolio/" width="100%vw" height="950px"  />
-         },
-        RootRoute::Route =>
-            html! {
-            <Switch<Route> render={Switch::render(switch)} />
-        },
-    }
-}
-
-
-
-fn switch(routes: &Route) -> Html {
-    match routes {
-        Route::About => html! { <p>{ "About" }</p> },
-        Route::NotFound => html! { <p>{ "Not Found" }</p> },
-    }
-}
-
-// ===================================================================================
-// for {username}.github.io
-
-// #[derive(Clone, Routable, PartialEq)]
-//  enum RootRoute {
-//      #[at("/")]
-//      Home,
-//      #[at("/about")]
-//      About,
-//      #[not_found]
-//      #[at("/404")]
-//      NotFound,
-//  }
-
-//  fn root_route(routes: &Route) -> Html {
-//      match routes {
-//          RootRoute::Home => html! { <p class="text-4xl">{ "Yew Template" }</p> },
-//          RootRoute::About => html! { <p>{ "About" }</p> },
-//          RootRoute::NotFound => html! { <p>{ "Not Found" }</p> },
-//      }
-//  }
-
-// ===================================================================================
-
-/// main root
-#[function_component(App)]
-fn app() -> Html {
     html! {
-        // ********************************************************
-        // **    basename is not supported on yew 0.19.0 yet.    **
-        // <BrowserRouter basename="/rust-example_yew-template/">
-        //     <Switch<Route> render={Switch::render(switch)} />
-        // </BrowserRouter>
-        // ********************************************************
-        <BrowserRouter>
-            <Switch<RootRoute> render={Switch::render(root_route)} />
-        </BrowserRouter>
+        <div>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
     }
 }
 
-/// entry point
 fn main() {
-    yew::start_app::<App>();
+    yew::Renderer::<App>::new().render();
 }
