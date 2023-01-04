f4b894ff8708ae3f8e4512216ea24e12462bb429
josephedward <josephedward@users.noreply.github.com>
2023-01-04 05:16:04 +0000

    deploy: 3074e4ea50515452391c0de7fe5cb415bfe07179

--- frontend-example-fac742fe4fe82d3c.js
+++ frontend-example-fac742fe4fe82d3c.js
@@ -620,7 +620,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('frontend-example-d70ce21c45892e4e_bg.wasm', import.meta.url);
+        input = new URL('frontend-example-fac742fe4fe82d3c_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
